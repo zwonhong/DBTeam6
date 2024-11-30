6d6a418c62848fcec152cfd5366a46ee8fce45f2
roychoi1012 <143794320+roychoi1012@users.noreply.github.com>
2024-11-30 23:01:42 +0900

    Update Twitter_Data.sql
    영어로 변경한 수정본입니다.

--- Twitter_Data.sql
+++ Twitter_Data.sql
@@ -13,83 +13,92 @@
 -- DROP TABLE IF EXISTS Articles;
 -- DROP TABLE IF EXISTS Following;
 -- DROP TABLE IF EXISTS Users;
+-- SET FOREIGN_KEY_CHECKS = 0;
+
+-- DELETE FROM Recomments;
+-- DELETE FROM Comments;
+-- DELETE FROM Articles;
+-- DELETE FROM Following;
+-- DELETE FROM Users;
+
+-- SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO Users (user_ID ,nickname, password, profilImg, wallPaper, introduction, birth, phoneNum, gender) 
 VALUES ('john03','john_doe', 'password123', 'profile.jpg', 'wallpaper.jpg', 'Hello, I am John!', '1990-05-15', '010-1234-5678', 'Male');
 
 INSERT INTO Users (user_ID, nickname, password) VALUES
-('Luffy', '루피', 'password1'),
-('Shinchan', '짱구','password2'),
-('Naruto', '나루토', 'password3');
+('Luffy', 'gomugomu', 'password1'),
+('Shinchan', 'buriburi','password2'),
+('Naruto', 'chidori', 'password3');
 
 INSERT INTO Following (following_ID, user_ID) VALUES
-('조로', 'Luffy'),
-('나미', 'Luffy'),
-('우솝', 'Luffy'),
-('상디', 'Luffy'),
-('로빈', 'Luffy'),
-('철수', 'Shinchan'),
-('유리', 'Shinchan'),
-('훈이', 'Shinchan'),
-('맹구', 'Shinchan'),
-('봉미선', 'Shinchan'),
-('신형만', 'Shinchan'),
-('사스케', 'Naruto'),
-('히나타', 'Naruto'),
-('사쿠라', 'Naruto'),
-('카카시', 'Naruto'),
-('마다라', 'Naruto');
+('Zoro', 'Luffy'),
+('Nami', 'Luffy'),
+('Usopp', 'Luffy'),
+('Sanji', 'Luffy'),
+('Robin', 'Luffy'),
+('Chulsoo', 'Shinchan'),
+('Yuri', 'Shinchan'),
+('Hooni', 'Shinchan'),
+('Maenggu', 'Shinchan'),
+('Bong Miseon', 'Shinchan'),
+('Shin Hyungman', 'Shinchan'),
+('Sasuke', 'Naruto'),
+('Hinata', 'Naruto'),
+('Sakura', 'Naruto'),
+('Kakashi', 'Naruto'),
+('Madara', 'Naruto');
 
 INSERT INTO Articles (articles_ID, user_ID, contents) VALUES
-('01', 'Luffy', '에이스 구출 성공 후 멋지게 사진 한 장'),
-('02', 'Shinchan', '아빠 엄마 짱아 흰둥이랑 함께라면 너무 행복해~'),
-('03', 'Naruto', '이루카 선생님 라면 감사하다니깐!!'),
-('04', 'Naruto', '미안하다 이거 보여주려고 어그로끌었다.. 나루토 사스케 싸움수준 ㄹㅇ실화냐? 진짜 세계관최강자들의 싸움이다..'),
-('05', 'Shinchan', '우리 짱아 납치한 놈 혼내주러 간다!'),
-('06', 'Luffy', '너 내 동료가 되라!'),
-('07', 'Luffy', '2년만에 모인 우리 해적단!'),
-('08', 'Luffy', '동료와 함께라면 그 무엇도 두렵지 않아'),
-('09', 'Shinchan', '떡잎마을 방범대 파이어!'),
-('10', 'Shinchan', '훈이랑 맹구 친해지길 바래~'),
-('11', 'Shinchan', '철수는 내 귓바람을 너무 좋아한단 말이야 ㅎㅎ'),
-('12', 'Naruto', '7반 때가 정말 좋았다니깐!'),
-('13', 'Naruto', '신 전설의 3닌자 출격'),
-('14', 'Naruto', '분신술은 너무 어려워;;'),
-('15', 'Shinchan', '우리 엄마는 슈퍼우먼이에요~');
+('01', 'Luffy', 'Ace rescue successful, taking a cool photo'),
+('02', 'Shinchan', 'So happy when I\'m with dad, mom, Jjanga, and Hwandungie~'),
+('03', 'Naruto', 'Iruka-sensei, thank you for the ramen!'),
+('04', 'Naruto', 'Sorry, I caused a fuss to show you this... Naruto vs Sasuke, is this real? The battle between the world\'s strongest!'),
+('05', 'Shinchan', 'I\'m going to punish the guy who kidnapped Jjanga!'),
+('06', 'Luffy', 'You must become my crew!'),
+('07', 'Luffy', 'Our pirate crew reunited after 2 years!'),
+('08', 'Luffy', 'Nothing to fear when I\'m with my crew'),
+('09', 'Shinchan', 'Ttak-Ip Village Defense Team, fire!'),
+('10', 'Shinchan', 'I hope Hooni and Maenggu become friends~'),
+('11', 'Shinchan', 'Chulsoo loves my ear whispers too much, haha'),
+('12', 'Naruto', 'The 7th squad was really great!'),
+('13', 'Naruto', 'The legendary 3 ninjas are back!'),
+('14', 'Naruto', 'Shadow clones are really hard;;'),
+('15', 'Shinchan', 'My mom is a superwoman~');
 
 INSERT INTO Comments (comment_ID, user_ID, article_ID, contents) VALUES
-('C01', 'Luffy', '01', '정말 멋진 사진이에요!'),
-('C02', 'Shinchan', '02', '짱구네 가족 너무 귀엽다!'),
-('C03', 'Naruto', '03', '라면 진짜 맛있겠네요!'),
-('C04', 'Naruto', '04', '싸움 장면 최고였습니다!'),
-('C05', 'Shinchan', '05', '우리 짱아 납치범 혼내줘야죠!'),
-('C06', 'Luffy', '06', '멋진 대사네요!'),
-('C07', 'Luffy', '07', '모두가 함께해서 좋아요!'),
-('C08', 'Luffy', '08', '모험은 역시 동료와 함께!'),
-('C09', 'Shinchan', '09', '떡잎마을 방범대 최고!'),
-('C10', 'Shinchan', '10', '훈이와 맹구는 왜 싸운 거죠?'),
-('C11', 'Shinchan', '11', '철수 귀엽다 ㅎㅎ'),
-('C12', 'Naruto', '12', '제7반 그립네요~'),
-('C13', 'Naruto', '13', '마다라는 진짜 강력했어요!'),
-('C14', 'Naruto', '14', '분신술 어렵지만 재밌어요~'),
-('C15', 'Shinchan', '15', '봉미선님은 정말 멋진 분!');
+('C01', 'Luffy', '01', 'Such a great photo!'),
+('C02', 'Shinchan', '02', 'Shinchan\'s family is so cute!'),
+('C03', 'Naruto', '03', 'The ramen looks really delicious!'),
+('C04', 'Naruto', '04', 'The fight scene was the best!'),
+('C05', 'Shinchan', '05', 'We need to punish the kidnapper of Jjanga!'),
+('C06', 'Luffy', '06', 'Great line!'),
+('C07', 'Luffy', '07', 'I\'m happy we\'re all together!'),
+('C08', 'Luffy', '08', 'Adventure is always better with the crew!'),
+('C09', 'Shinchan', '09', 'Ttak-Ip Village Defense Team is the best!'),
+('C10', 'Shinchan', '10', 'Why did Hooni and Maenggu fight?'),
+('C11', 'Shinchan', '11', 'Chulsoo is so cute, haha'),
+('C12', 'Naruto', '12', 'I miss the 7th squad~'),
+('C13', 'Naruto', '13', 'Madara was really powerful!'),
+('C14', 'Naruto', '14', 'Shadow clones are hard but fun~'),
+('C15', 'Shinchan', '15', 'Bong Miseon is really amazing!');
 
 INSERT INTO Recomments (recomment_ID, user_ID, article_ID, contents) VALUES
-('R01', 'Luffy', '01', '맞아요! 에이스는 정말 소중했죠!'),
-('R02', 'Shinchan', '02', '짱아도 귀엽고 흰둥이도 귀엽다~'),
-('R03', 'Naruto', '03', '라면은 언제 먹어도 맛있죠!'),
-('R04', 'Naruto', '04', '싸움 정말 인상 깊었어요!'),
-('R05', 'Shinchan', '05', '짱아 구출 작전 대박!'),
-('R06', 'Luffy', '06', '항상 동료가 우선입니다!'),
-('R07', 'Luffy', '07', '재회 장면 너무 감동적이었어요!'),
-('R08', 'Luffy', '08', '우정은 진짜 힘이 됩니다!'),
-('R09', 'Shinchan', '09', '방범대 모이면 항상 웃겨요 ㅋㅋ'),
-('R10', 'Shinchan', '10', '훈이랑 맹구 화해했나요?'),
-('R11', 'Shinchan', '11', '철수 귀여워 ㅎㅎ'),
-('R12', 'Naruto', '12', '제7반 최고죠~'),
-('R13', 'Naruto', '13', '마다라는 진짜 전설입니다!'),
-('R14', 'Naruto', '14', '분신술은 연습이 답이에요!'),
-('R15', 'Shinchan', '15', '봉미선님은 정말 대단하세요!');
+('R01', 'Luffy', '01', 'Yes, Ace was really precious!'),
+('R02', 'Shinchan', '02', 'Jjanga and Hwandungie are both so cute~'),
+('R03', 'Naruto', '03', 'Ramen is always delicious!'),
+('R04', 'Naruto', '04', 'The fight was really impressive!'),
+('R05', 'Shinchan', '05', 'The Jjanga rescue operation was awesome!'),
+('R06', 'Luffy', '06', 'My crew always comes first!'),
+('R07', 'Luffy', '07', 'The reunion scene was so emotional!'),
+('R08', 'Luffy', '08', 'Friendship really is powerful!'),
+('R09', 'Shinchan', '09', 'The Defense Team is always funny when they get together, haha'),
+('R10', 'Shinchan', '10', 'Did Hooni and Maenggu make up?'),
+('R11', 'Shinchan', '11', 'Chulsoo is so cute, haha'),
+('R12', 'Naruto', '12', 'The 7th squad is the best~'),
+('R13', 'Naruto', '13', 'Madara is truly a legend!'),
+('R14', 'Naruto', '14', 'Shadow clones are all about practice!'),
+('R15', 'Shinchan', '15', 'Bong Miseon is really amazing!');
 
 INSERT INTO Likes (user_ID, article_ID, comment_ID) VALUES
 ('Luffy', '01', 'C01'),
